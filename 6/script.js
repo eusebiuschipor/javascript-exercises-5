4bdb4a23369d3ab26c27102d968763592bf469a3
eusebiuschipor <eusebiu@eusebiuschipor.com>
2021-07-31 22:55:30 +0300

    Loops exercises

---
+++ 6/script.js
@@ -0,0 +1,4 @@
+/*
+    1) Define an array with 5 numbers. Using the for-of instruction, calculate the sum for all numbers in the array and show the sum in an alert
+    2) Define an object with the properties (name, city, country, age) and show in console all the properties from the object using the for-in instruction
+*/
